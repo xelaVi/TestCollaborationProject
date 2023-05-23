fc38d4056f9f860965d9627a5639cc752dddd081
aivanov <alex130896@gmail.com>
2023-05-24 00:13:55 +0400

    Initial Commit

---
+++ TestCollaborationProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCollaborationProject
+//
+//  Created by Александр Иванов on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
